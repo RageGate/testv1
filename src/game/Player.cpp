6b834d7fed4bf002b7f7151858be17014621d6d3
newrage <Bilndrage@hotmail.com>
2010-11-16 08:40:38 -0600

    pnv3 sd2 add sporrot for xp
    Signed-off-by: newrage <Bilndrage@hotmail.com>

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -2549,6 +2549,9 @@ void Player::GiveXP(uint32 xp, Unit* victim)
 
     if(!isAlive())
         return;
+    
+	if(HasFlag(PLAYER_FLAGS, PLAYER_FLAGS_XP_USER_DISABLED))
+       return;
 
     uint32 level = getLevel();
 
